9384dc856bd8138ac7cefbb7716be54e0790ec8e
Mateus Berlitz <wobetoberlitz@gmail.com>
2023-01-05 20:42:00 -0300

    adjusts to document, page title and icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -42,30 +42,31 @@ class MyDocument extends Document {
                             `,
                         }}
                     /> */}
+                    <link rel="shortcut icon" href="/icone_startril.svg" type="image/svg+xml"/>
 
                     <meta name="robots" content="index, follow"></meta>
 
-                    <meta name="description" content="Fornecemos o meio financeiro para você adquirir imóveis, veículos, investimentos e alavancar seu patrimônio."/>
-                    <link rel="icon" type="image/x-icon" href="./icon.svg"/>
+                    <meta name="description" content="Criamos Sites, Aplicações e Design para o seu objetivo."/>
+                    <link rel="icon" type="image/x-icon" href="./icone_startril.svg"/>
 
                     <meta name="facebook-domain-verification" content="g8yp6iz97nsgjl1dxn46e34gxaot0s"></meta>
                     <meta name="google-site-verification" content="Q5dGY-imD4ur2aZOKlLTgMCEIefxpB2AgWR36Ne2H2A"></meta>
 
-                    <meta name="category" content="Consórcio" />
-                    <link rel="index" title="Lance Consórcio" href="https://lanceconsorcio.com.br/"/>
-                    <link rel="logo" type="image/svg" href="./logo.svg"></link>
-                    <link rel="sitemap" type="application/xml" href="https://lanceconsorcio.com.br/sitemap.xml"></link>
+                    <meta name="category" content="Webdesign" />
+                    <link rel="index" title="Startril Webdesign" href="https://startril.com.br/"/>
+                    <link rel="logo" type="image/svg" href="./icone_startril.svg"></link>
+                    <link rel="sitemap" type="application/xml" href="https://startril.com.br/sitemap.xml"></link>
 
                     {/* <meta name="url" content="https://lanceconsorcio.com.br/"></meta> */}
                     {/* <meta property="og:url" content="https://old.lanceconsorcio.com.br/blog"></meta> */}
 
-                    <meta property="og:site_name" content="Lance Consórcio"></meta>
+                    <meta property="og:site_name" content="Startril Webdesign"></meta>
                     <meta property="og:locale" content="pt_BR"></meta>
-                    <meta property="og:title" content="Lance Consórcio - Plano de imóveis, veículos e investimentos"></meta>
-                    <meta property="og:description" content="Saiba como aproveitar mais do seu consórcio, construir seu patrimônio, adquirir bens, obter rendimentos, investir e todas as possibilidades!"></meta>
+                    <meta property="og:title" content="Startril Webdesign - Criação de sites, telas e sistemas"></meta>
+                    <meta property="og:description" content="Criamos Sites, Aplicações e Design para o seu objetivo."></meta>
 
                     <meta property="og:image" content="./images/card.jpg"></meta>
-                    <meta property="og:image:alt" content="Lance Consórcio - Plano de imóveis, veículos e investimentos"></meta>
+                    <meta property="og:image:alt" content="Startril Webdesign - Criação de sites, telas e sistemas"></meta>
                     <meta property="og:app_id" content="ns67765jnmz8"></meta>
                     <meta property="og:type" content="website"></meta>
                 </Head>
