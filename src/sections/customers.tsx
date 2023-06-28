47a991fcec9bf7a334b0a6dc6432c211e39f03b0
Mateus Berlitz <wobetoberlitz@gmail.com>
2023-06-28 14:46:16 -0300

    testing svg safari support

--- src/sections/customers.tsx
+++ src/sections/customers.tsx
@@ -52,7 +52,10 @@ export function Customers(){
                                     </Flex>
                                 </Flex>
                                 <Flex bg="rgba(255,255,255,0.05)" p="6" h="100px" justifyContent={"center"} backdropFilter={"blur(10px)"}>
-                                    <Flex maxW="120px" w='100%'><Lance/></Flex>
+                                    <Flex maxW="120px" w='100%'>
+                                        {/* <Lance/> */}
+                                        <object data="/lance.svg" type="image/svg+xml"></object>
+                                    </Flex>
                                 </Flex>
                                 <Flex bg="rgba(255,255,255,0.05)" p="6" h="100px" justifyContent={"center"} backdropFilter={"blur(10px)"}>
                                     <Flex maxW="140px" w='100%'><Ane/></Flex>
