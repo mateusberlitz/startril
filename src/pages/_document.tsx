508ad65ebb7711f9f07b4f8185f295db525fe7c4
Mateus Berlitz <wobetoberlitz@gmail.com>
2023-07-13 13:37:52 -0300

    added hotjar

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -46,8 +46,6 @@ class MyDocument extends Document {
                     <script
                         dangerouslySetInnerHTML={{
                             __html: `
-                                <!-- Hotjar Tracking Code for https://www.startril.com.br -->
-                                <script>
                                     (function(h,o,t,j,a,r){
                                         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
                                         h._hjSettings={hjid:3572485,hjsv:6};
@@ -56,7 +54,6 @@ class MyDocument extends Document {
                                         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
                                         a.appendChild(r);
                                     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-                                </script>
                             `,
                         }}
                     />
