6525c37b6204ea4258bdd260bf37a35e80c3ea7b
Mateus Berlitz <wobetoberlitz@gmail.com>
2023-08-22 13:57:42 -0300

    fixed apollo error

--- src/services/apollo.ts
+++ src/services/apollo.ts
@@ -1,32 +1,32 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { setContext } from 'apollo-link-context';
-import { createHttpLink } from "apollo-link-http";
+// import { ApolloClient, InMemoryCache } from "@apollo/client";
+// import { setContext } from 'apollo-link-context';
+// import { createHttpLink } from "apollo-link-http";
 
-const httpLink = createHttpLink({
-    uri: process.env.BLOG_URL,
-});
-
-const authLink = setContext((_, { headers }) => {
-    return {
-      headers: Object.assign(
-        headers || {},
-        {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json',
-          'Authorization': `Bearer ${process.env.BLOG_TOKEN}`,
-        }
-      )
-    }
-});
-
-const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-});
-
-// const client = new ApolloClient({
-//     uri: "https://api-sa-east-1.hygraph.com/v2/clgl3buh53sfn01t20jvxddmi/master",
-//     cache: new InMemoryCache()
+// const httpLink = createHttpLink({
+//     uri: process.env.BLOG_URL,
 // });
 
-export default client;
+// const authLink = setContext((_, { headers }) => {
+//     return {
+//       headers: Object.assign(
+//         headers || {},
+//         {
+//           'Content-Type': 'application/json',
+//           'Accept': 'application/json',
+//           'Authorization': `Bearer ${process.env.BLOG_TOKEN}`,
+//         }
+//       )
+//     }
+// });
+
+// const client = new ApolloClient({
+//     link: authLink.concat(httpLink),
+//     cache: new InMemoryCache(),
+// });
+
+// // const client = new ApolloClient({
+// //     uri: "https://api-sa-east-1.hygraph.com/v2/clgl3buh53sfn01t20jvxddmi/master",
+// //     cache: new InMemoryCache()
+// // });
+
+// export default client;
